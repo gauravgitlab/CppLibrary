4be5e20100197c526e4ec2167fc22803dfbfb125
LAPTOP-LOJHGVFB\gaurav <gauravthailand@gmail.com>
2022-08-14 13:47:36 +0100

    Basic C++ learing

---
+++ HelloWorld/Math.cpp
@@ -0,0 +1,30 @@
+
+int Multiply(int a, int b) {
+	int result = a * b;
+	return result;
+}
+
+
+
+//========================================================================================
+// with use of End Brace
+
+//int Multiply(int a, int b) {
+//	int result = a * b;
+//	return result;
+//#include "EndBrace.h"
+
+
+
+//========================================================================================
+// to include the code use 1, which is true or 
+// to exclude the code use 0 which is false
+
+//#if 1
+//int Multiply(int a, int b) {
+//	int result = a * b;
+//	return result;
+//}
+//#endif
+
+
---
+++ HelloWorld/x64/Debug/Math.i
@@ -0,0 +1,14 @@
+#line 1 "D:\\CppLibrary\\HelloWorld\\HelloWorld\\Math.cpp"
+
+
+
+
+
+
+
+int Multiply(int a, int b) {
+	int result = a * b;
+	return result;
+#line 1 "D:\\CppLibrary\\HelloWorld\\HelloWorld\\EndBrace.h"
+}
+#line 12 "D:\\CppLibrary\\HelloWorld\\HelloWorld\\Math.cpp"
